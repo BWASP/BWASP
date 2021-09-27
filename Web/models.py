f1a52deeacbf95c586e491048e03c66e80e9bbf1
isanghyeon <cpd4268@naver.com>
2021-09-27 21:05:00 +0900

    Update models.py

--- Web/models.py
+++ Web/models.py
@@ -2,9 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     __tablename__ = 'user'
-    
+
     num = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.String(32))
     password = db.Column(db.String(128))
