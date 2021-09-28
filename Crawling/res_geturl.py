26fd74699a3cba027cdecfabbd4ff1503b15dd53
PecentZero <security428@korea.ac.kr>
2021-09-28 18:44:11 +0900

    로직 오류로 다수 수정

--- Crawling/res_geturl.py
+++ Crawling/res_geturl.py
@@ -1,6 +1,6 @@
 from seleniumwire import webdriver
 import re
-
+from urllib.parse import urlparse
 
 #respone allow contenttype list
 #response allow extension list 
@@ -8,6 +8,7 @@ import re
 res_contlist=["javascript","json"]
 res_extlist=["js"]
 res_urllist=set()
+res_exturllist=set()
 res_jsonlist=set()
 res_jslist=set()
 
@@ -23,7 +24,7 @@ chrome_options.add_argument('--headless')
 
 driver = webdriver.Chrome(executable_path='chromedriver', options=chrome_options,seleniumwire_options=options)
 
-URL = "https://twitter.com/"
+URL = "https://twitter.com"
 driver.get(URL)
 
 #url 받을시 형태에 따라 저장 
@@ -33,58 +34,57 @@ driver.get(URL)
 
 def getExtraurl(body,url):
     #url regular expression
-    pattern = re.compile('(?:http|ftp|https)(?:://)([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?z')
+    #참고 정규식 pattern = re.compile('(http|ftp|https)(://)([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?z')
     #pattern = re.compile('(http|https)):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?')
-    print(url)
-    #print(body.decode("utf8"))
-    print("-"*10,"body's url start")
+    pattern = re.compile('(http|ftp|https)(://)([\w_-]+((\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?z')
+    
     try:
+        #regex group 에 따라 분리해서 출력 가능
+        #print("why ",line[0]+"://"+line[1]+line[2])
         for line in pattern.findall(body.decode("utf8")):
             print("extra link ","".join(line))
-            # regex group 에 따라 분리해서 출력 가능
-            #print("why ",line[0]+"://"+line[1]+line[2])
+            res_exturllist.add("".join(line))
+
     #utf-8이 오류날 경우        
     except:
+        #regex group 에 따라 분리해서 출력 가능
+        #print("why ",line[0]+"://"+line[1]+line[2])
          for line in pattern.findall(body.decode("ISO-8859-1")):
             print("extra link ","".join(line))
-            # regex group 에 따라 분리해서 출력 가능
-            #print("why ",line[0]+"://"+line[1]+line[2])
-
+            res_exturllist.add("".join(line))
+            
 
 #url 구분 저장 
 def saveUrl(type,body,url):
-    # 확장자 일 경우 확장자에 따라 타입 정하기 
+    # 확장자 일 경우 확장자에 따라 타입 정하기
     if type == "ext":
-        extension = url.split(".")[-1]
+        extension = urlparse(url).path.split(".")[-1]
         if extension == "json":
             type="json"
         elif extension == "js":
             type="javasript"
-    
+
     if type == "json":
             res_jsonlist.add(url)
-            print("json:"," ")
             getExtraurl(body,url)
 
     if type =="javascript":
-            res_jsonlist.add(url)
-            print("javascript:"," ")
+            res_jslist.add(url)
             getExtraurl(body,url)
 
 
 
 #reponse에서 js,json등 정해진 경우만 url 저장
 def getUrl(response,response_url):
-    content = request.response.headers['Content-Type']
+    content = response.headers['Content-Type']
     # 확장자 이상하지만 js , json일 경우 
     if content:
         for  contlist in res_contlist:
-            if content in contlist:
-                saveUrl(content,response.body,response_url)
+            if contlist in content:
+                saveUrl(contlist,response.body,response_url)
                 break
-    elif  "." in response_url:
+    elif  "." in urlparse(response_url).path:
         saveUrl("ext",response.body,response_url)
-
 
 
 
@@ -94,6 +94,11 @@ for request in driver.requests:
         res_urllist.add(request.url)
         getUrl(request.response,request.url)
 
-for list in sorted(list(res_urllist)):
-    print(list)    
+print("res url link result : ")
+for result in sorted(list(res_urllist)):
+    print(result)
+
+print("extra url link result :")
+for result in sorted(list(res_exturllist)):
+    print(result)    
 driver.quit()
