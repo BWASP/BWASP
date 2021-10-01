b170b7a1ce9c01f1fadb3bcebcb5a83bcb24bbfd
PecentZero <security428@korea.ac.kr>
2021-10-02 03:16:56 +0900

    JS 에서 location.href url 획득 기능 getjsExraurl 추가

--- Crawling/feature/res_geturl.py
+++ Crawling/feature/res_geturl.py
@@ -1,5 +1,6 @@
 from seleniumwire import webdriver
 from urllib.parse import urlparse
+from urllib.parse import urljoin
 import re
 
 res_contlist=["javascript","json","xml"]
@@ -10,21 +11,31 @@ res_jsonlist=set()
 res_jslist=set()
 res_xmllist=set()
 
-def getExtraurl(body,url):
+def getExtraurl(main_url,body,url):
     #url regular expression
     #참고 정규식 pattern = re.compile('(http|ftp|https)(://)([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?z')
     #pattern = re.compile('(http|https)):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?')
     pattern = re.compile('((?:http|ftp|https)(?:://)([\w_-]+((\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)')
-
     prefix = urlparse(url).scheme+"://"
     for line in pattern.findall(body):
         if not "http" in line[0]:
             res_exturllist.add(prefix+line[0])
         else:
             res_exturllist.add(line[0])
+
+def getjsExtraurl(main_url,body,url):
+    # location 포함 확인 pattern = re.compile('''(location.href)\s*[=\(]\s*["']["'\.\w\/\\\?=&\s\+]+;)''')
+    # location url 추출 
+    pattern = re.compile('''location.href\s*[=\(]\s*["'](["'\.\w\/\\\?=&\s\+]+);''')
+    #print("추출된 url",pattern.findall(body))
+    for line in pattern.findall(body):
+        line = re.sub('[\"\s+]','',line)
+        # urljoin을 통해 ./  ../  / 와 같은 상대경로 문제 해결
+        res_exturllist.add(urljoin(main_url,line))
+
             
 #url 구분 저장 
-def saveUrl(type,body,url):
+def saveUrl(main_url,type,body,url):
     # 확장자 일 경우 확장자에 따라 타입 정하기
     if type == "ext":
         extension=list()
@@ -39,31 +50,32 @@ def saveUrl(type,body,url):
             type="xml"
     if type == "json":
             res_jsonlist.add(url)
-            getExtraurl(body,url)
+            getExtraurl(main_url,body,url)
     if type =="javascript":
             res_jslist.add(url)
-            getExtraurl(body,url)
+            getExtraurl(main_url,body,url)
+            getjsExtraurl(main_url,body,url)
     if type =="xml":
             res_xmllist.add(url)
-            getExtraurl(body,url)
+            getExtraurl(main_url,body,url)
 
-def eachgetUrl(response,response_url):   
+def eachgetUrl(main_url,response,response_url):   
     if "content-type" in response["headers"]:
         content = response["headers"]["content-type"]
         for  contlist in res_contlist:
             if contlist in content:
-                saveUrl(contlist,response["body"],response_url)
+                saveUrl(main_url,contlist,response["body"],response_url)
                 break
     elif  "." in response_url:
-        saveUrl("ext",response["body"],response_url)
+        saveUrl(main_url,"ext",response["body"],response_url)
 
 #Call this to get extra link
-def getUrl(main_link,req_res_packet):
-    print("현재 링크 :",main_link)
+def getUrl(main_url,req_res_packet):
+    print("현재 링크 :",main_url)
     for request in req_res_packet:
         # 탐색된 모든 url 저장
         #res_urllist.add(request[i]["request"]["url"])
-        eachgetUrl(request["response"],request["request"]["full_url"])
+        eachgetUrl(main_url,request["response"],request["request"]["full_url"])
     return sorted(list(res_exturllist))
 
 def printUrl():
