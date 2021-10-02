bb1ce186539c7e1ba4b5eb54592d867ed4618805
Universe1122 <whdals7979@gmail.com>
2021-10-02 14:18:09 +0900

    scouter.py 코드 수정

--- Crawling/scouter.py
+++ Crawling/scouter.py
@@ -6,7 +6,7 @@ from feature import res_geturl
 
 def start(url, depth):
     driver = initSelenium()
-    visit(driver, url, set(), depth)
+    visit(driver, url, set(url), depth)
 
 def visit(driver, url, previous_url, depth):
     if depth == 0:
@@ -16,13 +16,18 @@ def visit(driver, url, previous_url, depth):
 
     cur_page_links = clickable_tags.seleniumCrawling(driver)
     req_res_packets = packet_capture.packetCapture(driver)
-    cur_page_links.append(res_geturl.getUrl(req_res_packets))
+    cur_page_links += res_geturl.getUrl(req_res_packets)
 
+    """
+    TODO
+        -> valid url 인지 확인 필요
+        -> 방문 전, 같은 도메인 인지 확인 필요
+    """
     for url in cur_page_links:
         if url in previous_url:
             continue
         else:
-            visit(driver, url, previous_url.add(cur_page_links), depth-1)
+            visit(driver, url, previous_url.union(cur_page_links), depth-1)
 
 def initSelenium():
     options = {
