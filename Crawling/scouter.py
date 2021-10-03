0050b37dbcb6b948c698d5a849f43897198e3f5a
dohunny <dhkoo.hacker@gmail.com>
2021-10-03 18:36:06 +0900

    clickable_tags.py 함수명 수정

--- Crawling/scouter.py
+++ Crawling/scouter.py
@@ -15,8 +15,8 @@ def visit(driver, url, previous_url, depth):
     
     driver.get(url)
 
-    cur_page_links = clickable_tags.seleniumCrawling(driver)
     req_res_packets = packet_capture.packetCapture(driver)
+    cur_page_links = clickable_tags.bs4Crawling(url, req_res_packets)
     cur_page_links += res_geturl.getUrl(url, req_res_packets)
     
     # packet_capture.writeFile(req_res_packets)
