6a0e1d9b073921396fad9bb12267f8efb1f45876
dohunny <dhkoo.hacker@gmail.com>
2021-10-03 16:57:44 +0900

    get_ports.py 정상 동작 확인

--- Crawling/feature/get_ports.py
+++ Crawling/feature/get_ports.py
@@ -1,25 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-def getSession():
-    cookies = {"polito":"\"8fef2320ef0254a734ff3e0994310ccb6153acd26153a86adf1899e8b8fae872!\""}
-    return cookies
-
 def getPorts(target_ip):
     open_ports = list()
+    html = BeautifulSoup(requests.get("https://censys.io/ipv4/" + target_ip).text, features="html.parser")
 
-    html = BeautifulSoup(requests.get("https://www.shodan.io/host/" + target_ip, cookies=getSession()).text, features="html.parser")
-    
-    print(html)
-    input()
-
-    for a_tag in html.select('#ports > a'):
-        print(a_tag.get_text())
-        open_ports.append(a_tag.get_text())
+    for i in html.select('h2'):
+        open_ports.append(i.get_text().replace('\n','').split('/')[0])
 
     return open_ports
 
 
 if __name__ == '__main__':
-    print(getPorts('223.130.195.95'))
+    print(getPorts('158.247.214.182'))
