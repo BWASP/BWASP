654a782b3565bc88e1047a300f8b63f412095480
dohunny <dhkoo.hacker@gmail.com>
2021-10-03 18:32:47 +0900

    fixed - #9

--- Crawling/feature/clickable_tags.py
+++ Crawling/feature/clickable_tags.py
@@ -1,12 +1,13 @@
 # https://marketology.co.kr/all-category/tag-manager/gtm-%ED%81%B4%EB%A6%AD-%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EC%B6%94%EC%A0%81%EC%9D%98-%EC%A0%95%EC%9D%98-%EB%AA%A9%EC%A0%81-%EB%B0%A9%EB%B2%95/
 
-import requests
+import requests, ast
 from bs4 import BeautifulSoup
 from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from urllib.parse import urlparse
 
 '''
 def parsingURL(url):
@@ -30,11 +31,12 @@ def seleniumSetting(url):
 def seleniumCrawling(driver):
 
     # 가져올 태그 목록
-    clickable_tag_list = ['button','a','div','img','input'] 
+    clickable_tag_types = ['button','a','div','img','input'] 
     cur_page_links, elems = list(), list()
 
-    for tag in clickable_tag_list:
+    for tag in clickable_tag_types:
         # 찾을 tag가 전부 로드 될 때까지 기다리기
+        
         WebDriverWait(driver, 10).until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR,tag))
         )
@@ -56,35 +58,60 @@ def seleniumCrawling(driver):
     return cur_page_links
 
 
+def checkURLFormat(origin, url):
+    
+    parsed_origin = urlparse(origin)
+    parsed_url = urlparse(url)
+
+    if parsed_url.scheme == '':
+        parsed_url = parsed_url._replace(scheme=parsed_origin.scheme)
+        parsed_url = urlparse(parsed_url.geturl())
+        print(parsed_url.geturl())
+
+    if parsed_url.netloc == '':
+        if parsed_url.path.split('/')[0] is not '':
+            parsed_url = parsed_url._replace(
+                scheme=parsed_origin.scheme, 
+                netloc=parsed_url.path.split('/')[0],
+                path=parsed_url.path[parsed_url.path.find('/'):]
+            )
+        else:
+            parsed_url = parsed_url._replace(netloc=parsed_origin.netloc)
+            
+    return parsed_url.geturl()
+
+
 # 2. BeautifulSoup을 이용한 태그 클릭 리스트
-def bs4Crawling(url):
-    html = requests.get(url).text
+def bs4Crawling(url, req_res_packets):
+    html = ''
+    for packet in req_res_packets:
+        # print(packet["request"]["full_url"])
+        if packet["request"]["full_url"] == url:
+            html = packet["response"]["body"]
 
     soup = BeautifulSoup(html, features="html.parser")
 
-    tags = list()
+    clickable_tag_types = ['button','a','div','img','input'] 
+    tags, cur_page_links = list(), list()
 
-    tags.append(soup.find_all('a'))
-    tags.append(soup.find_all('button'))
-    tags.append(soup.find_all('img'))
-    tags.append(soup.find_all('div'))
-    tags.append(soup.find_all('input'))
-
-    a_href_list = list()
-
+    for tag_type in clickable_tag_types:
+        tags.append(soup.find_all(tag_type))
+    
     for index in range(len(tags)):
         for item in tags[index]:
             try:
                 if item['href']:
-                    a_href_list.append(item['href'])
+                    cur_page_links.append(checkURLFormat(url, item['href']))
             except KeyError as e:
-                print(e)
+                # print(e)
+                pass
 
-    # print(a_href_list)
-    return a_href_list
+    return cur_page_links
 
 
 if __name__ == "__main__":
-    url = "https://www.naver.com/"
-    driver = seleniumSetting(url)
-    seleniumCrawling(driver)
+    url = "https://www.kitribob.wiki/"
+    req_res_packets = open('./Crawling/feature/message.txt','r').read()
+    req_res_packets = ast.literal_eval(req_res_packets)
+    # driver = seleniumSetting(url)
+    print(bs4Crawling(url, req_res_packets))
