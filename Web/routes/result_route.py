89ddfce13e7d1cc7bee08da925965ec42c44021f
Joon <jeongjy0317@gmail.com>
2021-10-04 17:24:43 +0900

    Modulized to components.

--- Web/routes/result_route.py
+++ Web/routes/result_route.py
@@ -9,7 +9,7 @@ bp = Blueprint(NAME, __name__, url_prefix='/')
 @bp.route('/')
 @bp.route('/index')
 def index():
-    return render_template('index.html', Title="홈 - BWASP")
+    return render_template('index.html', Title="Home - BWASP")
 
 
 @bp.route('/start')
