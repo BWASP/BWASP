8bccccf18f46197db4b15a68ffb428d524304151
isanghyeon <cpd4268@naver.com>
2021-10-04 19:39:13 +0900

    Update ChartsDB

--- Web/models/models.py
+++ Web/models/models.py
@@ -2,9 +2,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
+"""
 class User(db.Model):
     __tablename__ = 'user'
+
+    num = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.String(32))
+    password = db.Column(db.String(128))
+"""
+
+
+class pie(db.Model):
+    __tablename__ = 'pie'
+
+    num = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.String(32))
+    password = db.Column(db.String(128))
+
+
+class area(db.Model):
+    __tablename__ = 'area'
 
     num = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.String(32))
