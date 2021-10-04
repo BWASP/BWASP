8902242abf2dd16090d9b67308a9091aba9d6bf2
isanghyeon <cpd4268@naver.com>
2021-10-04 20:54:44 +0900

    Update app.py

--- Web/app.py
+++ Web/app.py
@@ -4,8 +4,9 @@
 import os
 from models.models import db
 from flask import (
-    Flask
+    Flask, render_template, g
 )
+from sqlalchemy.orm import sessionmaker
 
 
 def create_app(config=None):
@@ -15,16 +16,26 @@ def create_app(config=None):
     config = DevelopmentsConfig()
     app.config.from_object(config)
 
-    db.init_app(app)
-    db.app = app
-    db.create_all()
-
     # route initialize
     from routes import result_route, automation_route, common_route, api_route
     app.register_blueprint(result_route.bp)
     app.register_blueprint(automation_route.bp)
     app.register_blueprint(common_route.bp)
     app.register_blueprint(api_route.bp)
+
+    @app.before_first_request
+    def before_first_request():
+        db.init_app(app)
+        db.app = app
+        db.create_all()
+
+    @app.before_request
+    def before_request():
+        db.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+
+    @app.errorhandler(404)
+    def NotFound(error):
+        return render_template('404.html'), 404
 
     return app
 
