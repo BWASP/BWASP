a5989d8d83151653080a6f78d23a85a9e39ced3f
isanghyeon <cpd4268@naver.com>
2021-10-04 20:55:10 +0900

    Update configs.py

--- Web/configs.py
+++ Web/configs.py
@@ -5,7 +5,6 @@ BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 class Config(object):
     SECRET_KEY = os.urandom(16)
-    DEBUG = True
     SESSION_COOKIE_NAME = 'BWASP'
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_PATH, "databases/BWASP.db")}'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
