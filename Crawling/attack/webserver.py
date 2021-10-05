1fa78fd6074350034bce323358d2d86d9bcb3bf4
Universe1122 <whdals7979@gmail.com>
2021-10-05 15:49:14 +0900

    target webserver info 추출 기능

--- Crawling/attack/webserver.py
+++ Crawling/attack/webserver.py
@@ -2,17 +2,18 @@ import json
 import re
 from urllib.parse import urlparse
 
-category = [22]
+category = [22, 28, 33, 34, 46]
 
 def getWebServerInfo(target_url, req_res_packets):
+    return_data = {}
     data = loadCategory(category)
     
-    for packet in req_res_packets:
+    for index, packet in enumerate(req_res_packets):
         if not isSameDomain(target_url, packet["request"]["full_url"]):
             continue
         
         # check response Header
-        for key in data:
+        for key in list(data.keys()):
             if not "headers" in list(data[key].keys()):
                 continue
 
@@ -23,10 +24,30 @@ def getWebServerInfo(target_url, req_res_packets):
                 regex = data[key]["headers"][header]
                 regex = regex.replace("\\;version:\\1", "")
                 
-                pattern = re.compile(regex)
-                if pattern.search(packet["response"]["headers"][header.lower()]) != None:
-                    print("Server: {}".format(key))
-                    print("Packet: {}\n\n\n".format(json.dumps(packet["response"]["headers"], indent=4)))
+                # Find Server Info
+                server_pattern = re.compile(regex)
+                server_info = server_pattern.search(packet["response"]["headers"][header.lower()])
+                if server_info != None:
+                    if not key in list(return_data.keys()):
+                        return_data[key] = {
+                            "detect" : [],
+                            "version" : "",
+                            "request" : [],
+                            "response" : []
+                        }
+
+                    if not header in return_data[key]["detect"]:
+                        return_data[key]["detect"].append(header)
+
+                    return_data[key]["response"].append(index)
+
+                    # Find Server Version Info
+                    version_pattern = re.compile('([\d.]+)')
+                    version_info = version_pattern.search(server_info.group())
+                    if version_info != None:
+                        return_data[key]["version"] = version_info.group()
+    
+    return return_data
 
 def loadCategory(category):
     return_data = {}
@@ -62,4 +83,5 @@ if __name__ == "__main__":
     data = json.load(f)
     f.close()
 
-    getWebServerInfo("https://www.casper.or.kr/", data)
+    result = getWebServerInfo("https://www.boannews.com/", data)
+    print(json.dumps(result, indent=4))
