069a3d9489be096e5004932b567b0412251dc7c9
dohunny <dhkoo.hacker@gmail.com>
2021-10-06 21:07:05 +0900

    포트스캔(Online/Offline) 기능 완성

--- Crawling/feature/get_ports.py
+++ Crawling/feature/get_ports.py
@@ -1,15 +1,53 @@
 import requests
+from socket import *
 from bs4 import BeautifulSoup
 
-def getPorts(target_ip):
-    open_ports = list()
+def getPortsOnline(target_ip):
+    open_ports = dict()
     html = BeautifulSoup(requests.get("https://censys.io/ipv4/" + target_ip).text, features="html.parser")
 
     for i in html.select('h2'):
-        open_ports.append(i.get_text().replace('\n','').split('/')[0])
+        port = i.get_text().replace('\n','').split('/')[0]
+        service = getservbyport(int(port))
+        open_ports[str(port)] = service
 
     return open_ports
 
+def getPortsOffline(target_ip):
+    try:
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.connect((target_ip,80))
+        ipscan = s.getsockname()[0]
+
+        print("[+] IP : " +ipscan)
+
+        port = [80, 8080, 443, 20, 21, 22, 23, 25, 53, 5357, 110, 123, 161, 1433, 3306, 1521, 135, 139, 137, 138, 445, 514, 8443, 3389, 8090, 42, 70, 79, 88, 118, 156, 220]
+        host = target_ip
+
+        target_ip = gethostbyname(host)
+        opened_ports = dict()
+        service_data = {'3306': 'mysql', '1521': 'oracle', '8080': 'tomcat', '8443': 'https', '8090': 'tomcat', '220': 'imap3'}
+
+        for p in port:
+            sock = socket(AF_INET, SOCK_STREAM)
+            sock.settimeout(1)
+            result = sock.connect_ex((target_ip, p))
+            try:
+                service = getservbyport(p)
+            except:
+                p = str(p)
+                service = service_data[""+p+""]
+            
+            if result == 0:
+                opened_ports[str(p)] = service
+                
+        for i in opened_ports:
+            print('[+] Open Port and Service : {}, {}'.format(i, opened_ports[i]))
+
+    except error as e:
+        print("[Error] - Port Scan\n" + e)
+    
+    return opened_ports
 
 if __name__ == '__main__':
-    print(getPorts('158.247.214.182'))
+    print(getPortsOnline('158.247.214.182').keys())
