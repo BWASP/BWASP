7d4210c0ca5a1252a65a63ff8423500443497f27
isanghyeon <cpd4268@naver.com>
2021-10-08 02:47:56 +0900

    Update Controller, Routes

---
+++ Web/__init__.py
@@ -0,0 +1,52 @@
+# Flask version 2.0.1
+# Flask-SQLAlchemy version 2.5.1
+
+from flask import (
+    Flask, render_template, g
+)
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app(config=None):
+    app = Flask(__name__)
+
+    from .configs import DevelopmentsConfig, ProductionConfig
+    config = DevelopmentsConfig()
+    """
+    if app.config['DEBUG']:
+        config = DevelopmentsConfig()
+    else:
+        config = ProductionConfig()
+    """
+
+    # config type
+    app.config.from_object(config)
+
+    # route initialize
+    from .routes import result_route, automation_route, common_route, api_route
+    app.register_blueprint(result_route.bp)
+    app.register_blueprint(automation_route.bp)
+    app.register_blueprint(common_route.bp)
+    app.register_blueprint(api_route.bp)
+
+    db.init_app(app)
+
+    @app.errorhandler(404)
+    def NotFound(error):
+        return render_template('404.html'), 404
+
+    @app.before_request
+    def before_request():
+        g.db = db.session
+
+    @app.teardown_request
+    def teardown_request(exception):
+        if hasattr(g, 'db'):
+            g.db.close()
+
+    return app
+
+    # if __name__ == '__main__':
+    #    create_app().run(host='0.0.0.0', port=5000, debug=True)
