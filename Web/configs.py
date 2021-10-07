754a5ad61ff19ddb7e1cd264a86a5ba441e19315
isanghyeon <cpd4268@naver.com>
2021-10-08 02:48:08 +0900

    Update configs.py

--- Web/configs.py
+++ Web/configs.py
@@ -4,9 +4,18 @@ BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 class Config(object):
+    """
+        Flask Config
+    """
     SECRET_KEY = os.urandom(16)
     SESSION_COOKIE_NAME = 'BWASP'
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_PATH, "databases/BWASP.db")}'
+    """
+    SQLALCHEMY_BINDS = {
+        # "BWASP": f'sqlite:///{os.path.join(BASE_PATH, "databases/BWASP.db")}',
+        # "CHART": f'sqlite:///{os.path.join(BASE_PATH, "databases/CHART.db")}'
+    }
+    """
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -15,6 +24,14 @@ class Config(object):
 
 
 class DevelopmentsConfig(Config):
+    """
+        Flask Config for Development
+    """
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_PATH,"databases/CHART.db")}'
 
+
+class ProductionConfig(Config):
+    """
+        Flask Config for Production
+    """
+    pass
