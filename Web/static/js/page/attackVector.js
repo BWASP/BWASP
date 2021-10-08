27dea7bd31dfcee687d2f247f9d22ca464bc14d2
Joon <jeongjy0317@gmail.com>
2021-10-08 23:22:53 +0900

    Added support for transition between packet and attack vectors.

--- Web/static/js/page/attackVector.js
+++ Web/static/js/page/attackVector.js
@@ -1,5 +1,5 @@
 // true (vector) / false (packet)
-let currentState = true;
+let currentState = false;
 let implementationSample_attackVector = [
     {
         url: "http://bobmart.com/main.php",
@@ -37,28 +37,31 @@ let implementationSample_packets = [
         impactRate: 0
     }
 ];
+let idKeyList = [], key = "";
 
-console.log
+let createKey = () => {
+    let gen = () => {
+        let key = Math.random().toString(36).substring(2);
+        if(!idKeyList.includes(key)) return key;
+        else gen()
+    }
+    return `anonID-${gen()}-${gen()}`;
+}
 
 document.getElementById("switchToPacket").addEventListener("click", function(){
+    let table = {
+        table: document.createElement("table"),
+        thead: document.createElement("thead"),
+        tbody: document.createElement("tbody")
+    };
     currentState = !currentState;
     let status = (currentState) ? "Attack Vector" : "Packets";
-    let idKeyList = [], key = "";
-    let createKey = () => {
-        let gen = () => {
-            key = Math.random().toString(36).substring(2);
-            if(!idKeyList.includes(key)) return key;
-            else gen()
-        }
-        return `anonID-${gen()}-${gen()}`;
-    }
-    // for(let s=0; s<=500; s++) console.log(createKey());
     key = Math.random().toString(36).substring(2);
     document.getElementById("titleOfPage").innerHTML = status;
     document.title = `${status} - BWASP`;
 
     let thead = document.getElementById("parallelTableHead"),
-        table = $('#dataTable'),
+        tbody = document.getElementById("parallelTableBody"),
         newThead = document.createElement("tr"),
         newTbodyElements = [],
         element = [
@@ -68,18 +71,17 @@ document.getElementById("switchToPacket").addEventListener("click", function(){
         currentData = [];
 
     // Build thead
-    element[Number(currentState)].forEach((columnName)=>{
+    element[Number(!currentState)].forEach((columnName)=>{
         let tempThElement = document.createElement("th");
         tempThElement.innerHTML = columnName;
         newThead.appendChild(tempThElement);
     });
     // render thead
-    thead.innerHTML = "";
-    thead.appendChild(newThead);
+    table.thead.appendChild(newThead);
 
     // Build tbody
     let buildData = (currentState) ? implementationSample_attackVector : implementationSample_packets;
-    for(let count=0; count<buildData.length; count++){
+    for (let count = 0; count < buildData.length; count++) {
         let frame = document.createElement("tr");
         let localData = buildData[count], element = Object();
         // URL
@@ -97,25 +99,140 @@ document.getElementById("switchToPacket").addEventListener("click", function(){
                 }
             }
         };
+        let linebreak = document.createElement("br");
+
+        element.url.parent.classList.add("align-middle");
+
         element.url.child.url.href = `#${idKey}`;
         element.url.child.url.innerHTML = localData.url;
         element.url.child.url.classList.add("d-block", "py-3", "m-0", "font-weight-bold", "text-primary");
+        element.url.child.url.setAttribute("data-toggle", "collapse");
+        element.url.child.url.setAttribute("role", "button");
+        element.url.child.url.setAttribute("aria-expanded", "false")
+        element.url.child.url.setAttribute("aria-controls", idKey);
 
-        console.log(element.url.child.url);
-    }
-    // tbody - essential - URL
+        element.url.child.dropdown.parent.classList.add("collapse");
+        element.url.child.dropdown.parent.id = idKey;
 
+        element.url.child.dropdown.child.title.classList.add("font-weight-bold", "mb-1");
+        element.url.child.dropdown.child.title.innerText = "Payloads";
 
+        localData.payloads.forEach((payload) => {
+            let liElement = document.createElement("li");
+            liElement.innerText = payload;
+            element.url.child.dropdown.child.contents.appendChild(liElement);
+        })
+        element.url.child.dropdown.parent.append(
+            element.url.child.dropdown.child.title,
+            element.url.child.dropdown.child.contents
+        );
+        element.url.parent.append(
+            element.url.child.url,
+            element.url.child.dropdown.parent
+        );
+        frame.appendChild(element.url.parent);
 
-    // Initialize datatables
-    table.DataTable();
-
-
-
-    if(currentState){
-        // Attack vector
-
-    }else{
         // Packets
+        if(!currentState){
+            let packetDOM = document.createElement("td");
+            packetDOM.innerText = localData.packet;
+            packetDOM.classList.add("align-middle");
+            frame.appendChild(packetDOM);
+        }
+
+        // Vulnerability doubt
+        element["vuln"] = {
+            parent: document.createElement("td"),
+            child: {
+                vulnType: document.createElement("span")
+            }
+        }
+        element.vuln.parent.classList.add("align-middle");
+        frame.appendChild(element.vuln.parent);
+
+        // Method
+        element["method"] = document.createElement("td");
+        element.method.innerText = localData.method;
+        element.method.classList.add("align-middle");
+        frame.appendChild(element.method);
+
+        // Related
+        if(!currentState){
+            let relatedData = {
+                parent: document.createElement("td"),
+                child: document.createElement("div")
+            };
+            relatedData.parent.classList.add("align-middle");
+            localData.relatedData.forEach((data)=>{
+                let relatedDOM = document.createElement("a")
+                relatedDOM.href = data;
+                relatedDOM.innerHTML = data;
+                relatedData.child.appendChild(relatedDOM);
+            })
+            relatedData.parent.appendChild(relatedData.child);
+            frame.appendChild(relatedData.parent);
+        }
+
+        let date = new Date(localData.date);
+        date = date.toISOString().substring(0, 19).split("T")
+        date.push("UTC");
+        element["date"] = {
+            parent: document.createElement("td"),
+            child: {
+                date: document.createElement("span"),
+                time: document.createElement("span")
+            }
+        }
+        element.date.parent.classList.add("align-middle");
+        element.date.child.date.classList.add("font-weight-bold");
+        element.date.child.date.innerText = date[0];
+        element.date.child.time.innerText = `${date[1]} ${date[2]}`;
+        element.date.parent.append(
+            element.date.child.date,
+            linebreak,
+            element.date.child.time
+        );
+        frame.appendChild(element.date.parent);
+
+        // Impact
+        element["impact"] = {
+            parent: document.createElement("td"),
+            child: document.createElement("p")
+        };
+        let impactData = [];
+        switch(localData.impactRate){
+            case 0:
+                impactData = ["success", "Low"]
+                break
+            case 1:
+                impactData = ["warning", "Normal"]
+                break
+            case 2:
+                impactData = ["danger", "High"]
+                break
+        }
+
+        element.impact.parent.classList.add("align-middle");
+        element.impact.child.classList.add("btn", "btn-icon-split", "p-1", "mb-0", "pl-3", "pr-3", "rounded-pill", `btn-${impactData[0]}`);
+        element.impact.child.innerText = impactData[1];
+
+        element.impact.parent.appendChild(element.impact.child);
+        frame.appendChild(element.impact.parent);
+
+        table.tbody.appendChild(frame);
     }
+
+    let tableID = createKey(),
+        tablePlace = document.getElementById("tablePlace");
+    table.table.classList.add("table", "table-bordered");
+    table.table.id = tableID;
+    table.table.append(
+        table.thead,
+        table.tbody
+    );
+    tablePlace.innerHTML = "";
+    tablePlace.appendChild(table.table);
+    $(`#${tableID}`).DataTable();
 })
+
+window.onload = () => document.getElementById("switchToPacket").click();
