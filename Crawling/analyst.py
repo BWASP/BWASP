3d91921469f18439ebf8b9ab8da358b195710137
PecentZero <security428@korea.ac.kr>
2021-10-31 02:46:27 +0900

    detect_list 전역 변수 전달

--- Crawling/analyst.py
+++ Crawling/analyst.py
@@ -1,5 +1,5 @@
 from Crawling.attack import sysinfo
 
 
-def start(target_url, req_res_packets, cur_page_links, options):
-    return sysinfo.start(target_url, cur_page_links, req_res_packets,options["framework"])
+def start(detect_list,target_url, req_res_packets, cur_page_links, options):
+    return sysinfo.start(detect_list,target_url, cur_page_links, req_res_packets,options["framework"])
