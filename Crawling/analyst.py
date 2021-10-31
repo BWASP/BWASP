dd63c221f5c0698ff470ef3854fc2f5e2c986921
PecentZero <security428@korea.ac.kr>
2021-10-31 22:14:36 +0900

    sysinfo related 패킷 을 위한 packet_indexes 인자 추가

--- Crawling/analyst.py
+++ Crawling/analyst.py
@@ -1,5 +1,5 @@
 from Crawling.attack import sysinfo
 
 
-def start(detect_list,target_url, req_res_packets, cur_page_links, options):
-    return sysinfo.start(detect_list,target_url, cur_page_links, req_res_packets,options["framework"])
+def start(detect_list,target_url, req_res_packets, cur_page_links, packet_indexes,options):
+    return sysinfo.start(detect_list,target_url, cur_page_links, req_res_packets,packet_indexes,options["framework"])
