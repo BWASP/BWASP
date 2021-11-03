046eb5cde698cc485714e725bb93459f039b6bbf
isanghyeon <cpd4268@naver.com>
2021-11-03 21:39:13 +0900

    Web server structure change

--- Web/app.py
+++ Web/app.py
@@ -1,16 +1,17 @@
-# Flask version 2.0.1
-# Flask-SQLAlchemy version 2.5.1
-
 from flask import (
-    Flask, render_template, g
+    Flask, render_template
 )
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
 from Crawling.scouter import start
 
 
 def create_app(config=None):
     app = Flask(__name__)
 
-    from .configs import DevelopmentsConfig, ProductionConfig
+    from configs import DevelopmentsConfig, ProductionConfig
     if app.config['DEBUG']:
         config = DevelopmentsConfig()
     else:
@@ -20,7 +21,7 @@ def create_app(config=None):
     app.config.from_object(config)
 
     # route initialization
-    from .routes import index_route, automation_route, common_route
+    from routes import index_route, automation_route, common_route
     app.register_blueprint(index_route.bp)
     app.register_blueprint(automation_route.bp)
     app.register_blueprint(common_route.bp)
@@ -34,3 +35,8 @@ def create_app(config=None):
 
 def AutomatedAnalysis(url, depth, options):
     start(url, int(depth), options)
+
+
+if __name__ == '__main__':
+    application = create_app()
+    application.run(host="0.0.0.0", port=5000, debug=True)
