df9a10da145b35b706f5a0a046b458655e97a126
isanghyeon <cpd4268@naver.com>
2021-11-03 22:49:38 +0900

    Update automation_route.py

--- Web/routes/automation_route.py
+++ Web/routes/automation_route.py
@@ -1,8 +1,7 @@
 from flask import (
-    Blueprint, render_template, g,
-    request, url_for, redirect, jsonify
+    Blueprint, render_template, request
 )
-from Web import AutomatedAnalysis
+from Web.app import AutomatedAnalysis
 import json
 
 NAME = 'automation'
@@ -11,5 +10,41 @@ bp = Blueprint(NAME, __name__, url_prefix='/automation')
 
 @bp.route('/options', methods=['GET', 'POST'])
 def manual_options():
-    AutomatedAnalysis(reqJsonData["target"]["url"], reqJsonData["tool"]["analysisLevel"], reqJsonData)
+    if request.method == 'POST':
+        reqJsonData = request.form["reqJsonData"]
+        AutomatedAnalysis(reqJsonData["target"]["url"], int(reqJsonData["tool"]["analysisLevel"]), reqJsonData["tool"]["optionalJobs"])
+
+        response = requests.post(
+            url="http://localhost:20102/api/job",
+            headers={"accept": "application/json", "Content-Type": "application/json"},
+            data={"targetURL": reqJsonData["target"]["url"], "knownInfo": reqJsonData["info"], "recursiveLevel": int(reqJsonData["tool"]["analysisLevel"]),
+                  "uriPath": reqJsonData["target"]["path"]}
+        )
+        print(response.status_code)
+
     return render_template('automation/options.html', Title="Option for Automated analysis - BWASP")
+
+
+"""
+def AutomatedAnalysis(url, depth, options):
+    start(url, int(depth), options)
+
+reqJsonData: 
+{
+  "tool": {
+    "analysisLevel": "1",
+    "optionalJobs": []
+  },
+  "info": {
+    "server": [],
+    "framework": [],
+    "backend": []
+  },
+  "target": {
+    "url": "http://testphp.vulnweb.com/",
+    "path": [
+      ""
+    ]
+  }
+}
+"""
