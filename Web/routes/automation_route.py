371ee3a6ff4ac070913a8b2f058b1869af5c686c
isanghyeon <cpd4268@naver.com>
2021-11-06 23:09:33 +0900

    Update automation_route.py

--- Web/routes/automation_route.py
+++ Web/routes/automation_route.py
@@ -2,6 +2,7 @@ from flask import (
     Blueprint, render_template, request, abort
 )
 from Web.app import AutomatedAnalysis
+import json, request
 
 NAME = 'automation'
 bp = Blueprint(NAME, __name__, url_prefix='/automation')
