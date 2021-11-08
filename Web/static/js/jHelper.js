304865f2b834692c8568cfea008218fce7441cd1
Joon <jeongjy0317@gmail.com>
2021-11-09 06:21:17 +0900

    Added tableBuilder module and changed API module to async.

--- Web/static/js/jHelper.js
+++ Web/static/js/jHelper.js
@@ -1,15 +1,14 @@
 class API {
     constructor() {
-        this.getConfig();
-    }
-
-    async getConfig() {
-        await fetch("/static/data/frontConfig.json")
-            .then(blob => blob.json())
-            .then(res => {
-                this.API = res.API
-                this.debug = res.debug
-            })
+        return (async () => {
+            await fetch("/static/data/frontConfig.json")
+                .then(blob => blob.json())
+                .then(res => {
+                    this.API = res.API
+                    this.debug = res.debug
+                })
+            return this;
+        })();
     }
 
     /**
@@ -18,26 +17,24 @@ class API {
      * @param {function} callback
      */
     communicate(endpoint, callback) {
-        this.getConfig().then(() => {
-            fetch(this.API.base + endpoint, {
-                method: "GET",
-                cache: "no-cache",
-                headers: {
-                    "Content-Type": "application/json",
-                    "accept": "application/json"
-                }
-            })
-                .then(blob => blob.json())
-                .then(res => {
-                    if (this.debug.mode === true
-                        && this.debug.functions.printAllOutput === true) console.log(`:: DEBUG : RETURN ::\n${res}`);
-                    callback(null, res);
-                })
-                .catch(error => {
-                    console.log(`:: DEBUG : ERROR ::\n${error}`);
-                    callback(error)
-                })
+        fetch(this.API.base + endpoint, {
+            method: "GET",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json",
+                "accept": "application/json"
+            }
         })
+            .then(blob => blob.json())
+            .then(res => {
+                if (this.debug.mode === true
+                    && this.debug.functions.printAllOutput === true) console.log(`:: DEBUG : RETURN ::\n${res}`);
+                callback(null, res);
+            })
+            .catch(error => {
+                console.log(`:: DEBUG : ERROR ::\n${error}`);
+                callback(error)
+            })
     }
 
     /**
@@ -76,7 +73,8 @@ class API {
      * @returns {string} Pure JSON
      */
     jsonDataHandler(str, parseJSON = true) {
-        let replaceKeyword = ["::SINGLE-QUOTE::","::DOUBLE-QUOTE::"];
+        if (typeof (str) === "object") return str;
+        let replaceKeyword = ["::SINGLE-QUOTE::", "::DOUBLE-QUOTE::"];
         str = str
             .replaceAll("\"", replaceKeyword[1])
             .replaceAll("\\'", replaceKeyword[0])
@@ -84,6 +82,41 @@ class API {
             .replaceAll(replaceKeyword[0], "\\'")
             .replaceAll(replaceKeyword[1], "\'");
         return (parseJSON) ? JSON.parse(str) : str;
+    }
+}
+
+class tableBuilder {
+    /**
+     * Builds table <thead> element
+     * @param {Array} elements
+     * @return {Object} thead
+     */
+    buildHead(elements) {
+        if (typeof (elements) !== "object" || elements.length === 0) return Error("tableBuilder.buildHead() : Expected array of heading elements");
+        let thead = {
+            parent: document.createElement("thead"),
+            child: document.createElement("tr")
+        };
+        elements.forEach((currentElement) => {
+            let tmpElement = document.createElement("th");
+            tmpElement.innerText = currentElement;
+            thead.child.appendChild(tmpElement);
+        })
+        thead.parent.appendChild(thead.child);
+        return thead.parent;
+    }
+
+    dataNotPresent(){
+        let noData = document.createElement("td");
+        noData.innerText = " - ";
+        noData.classList.add("text-muted", "text-center");
+        return noData;
+    }
+
+    commaAsElement(){
+        let comma = document.createElement("span");
+        comma.innerText = ", ";
+        return comma;
     }
 }
 
@@ -102,4 +135,4 @@ let createKey = (level = 2, keyPrefix = "anonID") => {
     return createdKey;
 }
 
-export {API, createKey};
+export {API, createKey, tableBuilder};
