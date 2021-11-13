d39db5410156ccf27346d6a633c8b57ff41964bb
PecentZero <security428@korea.ac.kr>
2021-11-14 04:56:11 +0900

    manual.py 기타 import 위치 수정 필요 , crx.py 함수 호출 필요 , 충분한 패킷으로 manual.py 테스트 필요 feat 주원 ,주명

--- ManualAPI/routes/crx.py
+++ ManualAPI/routes/crx.py
@@ -1,6 +1,8 @@
+import re
 from flask import (
-    Blueprint, g
+    Blueprint, g, request
 )
+from modules import manual
 
 NAME = 'crx'
 bp = Blueprint(NAME, __name__, url_prefix='/crx')
@@ -8,6 +10,9 @@ bp = Blueprint(NAME, __name__, url_prefix='/crx')
 
 @bp.route('/Send', methods=['GET', 'POST'])
 def Send():
+    if request.method == "GET":
+        return manual.start(data)
+
     return "/crx/Send"
 
 
