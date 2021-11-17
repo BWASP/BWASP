08dcdc92bd29e5c34786e8a9466d055a58a98b7d
isanghyeon <cpd4268@naver.com>
2021-11-17 16:01:38 +0900

    Update cve_info.py

--- API/apis/cve_info.py
+++ API/apis/cve_info.py
@@ -28,7 +28,7 @@ class Cve_information_data_access_object(object):
         self.cve_list = ""
         self.limitCount = 9
 
-    def get_search_cve_list(self, framework=None, version=None, Type=False):
+    def get_search_cve_list(self, framework=None, version=None):
         if framework is None and version is None:
             ns.abort(404, f"cve info doesn't exist; Your framework: {framework}, Version: {version}")
 
@@ -40,15 +40,21 @@ class Cve_information_data_access_object(object):
 
         self.counter = self.selectData.order_by(cveModel.year.desc()).limit(self.limitCount).count()
 
-        if self.counter == 0:
+        if self.counter < 1:
             ns.abort(404, f"cve info doesn't exist; Your framework: {framework}, Version: {version}")
 
         self.cve_list = self.selectData.order_by(cveModel.year.desc()).limit(self.limitCount).all()
+        return self.cve_list
 
-        if Type is False:
-            return self.cve_list
-        else:
-            return {"count": int(self.counter)}
+    def get_count_of_search_cve_list(self, framework=None, version=None):
+        self.selectData = g.cve_db_obj.query(cveModel).filter(
+            cveModel.description.like(f"%{framework}%")
+        ) if version == "0" else g.cve_db_obj.query(cveModel).filter(
+            cveModel.description.like(f"%{framework}%"), cveModel.description.like(f"%{version}%")
+        )
+
+        self.counter = self.selectData.order_by(cveModel.year.desc()).limit(self.limitCount).count()
+        return {"count": int(self.counter)}
 
 
 data_access_object_for_cve_information = Cve_information_data_access_object()
@@ -63,7 +69,7 @@ class Cve_list(Resource):
     @ns.marshal_list_with(cve_info)
     def get(self, framework, version):
         """List a CVE info"""
-        return data_access_object_for_cve_information.get_search_cve_list(framework=framework, version=version, Type=False)
+        return data_access_object_for_cve_information.get_search_cve_list(framework=framework, version=version)
 
 
 @ns.route('/<string:framework>/<string:version>/count')
@@ -74,4 +80,4 @@ class Count_of_cve_list(Resource):
     @ns.marshal_with(cve_info_count)
     def get(self, framework, version):
         """count a CVE info list"""
-        return data_access_object_for_cve_information.get_search_cve_list(framework=framework, version=version,  Type=True)
+        return data_access_object_for_cve_information.get_count_of_search_cve_list(framework=framework, version=version)
