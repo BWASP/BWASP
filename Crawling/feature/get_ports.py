da18a6dbc52ba44f13256fa3678618d3dc7e578a
isanghyeon <cpd4268@naver.com>
2021-11-21 17:26:21 +0900

    Update get_ports.py

--- Crawling/feature/get_ports.py
+++ Crawling/feature/get_ports.py
@@ -5,13 +5,13 @@ from bs4 import BeautifulSoup
 
 
 class GetPort:
-    def __int__(self):
+    def __init__(self):
         self.ports_list = dict()  # if port is open, get in
         self.wellKnown_ports = [80, 8080, 443, 20, 21, 22, 23, 25, 53, 5357, 110, 123, 161, 1433, 3306, 1521, 135, 139, 137, 138, 445, 514, 8443, 3389, 8090, 42, 70, 79, 88, 118,
                                 156, 220]
         self.service_of_port = {'3306': 'mysql', '1521': 'oracle', '8080': 'tomcat', '8443': 'https', '8090': 'tomcat', '220': 'imap3'}
 
-        self.port_pattern = '''^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\:(\d)+)?$'''
+        self.port_pattern = re.compile('''^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\:(\d)+)?$''')
         self.port_scan = {
             "online": {
                 "Get_port_information_url": "https://search.censys.io/hosts/",
@@ -26,9 +26,7 @@ class GetPort:
         }
 
     def getPortsOnline(self, target_ip):
-        port_pattern = re.compile(self.port_pattern)
-
-        if port_pattern.match(target_ip) is None:
+        if self.port_pattern.match(target_ip) is None:
             try:
                 target_ip = gethostbyname(urlparse(target_ip).netloc)
             except:
@@ -48,9 +46,7 @@ class GetPort:
         return self.ports_list
 
     def getPortsOffline(self, target_ip):
-        port_pattern = re.compile(self.port_pattern)
-
-        if re.search(port_pattern, target_ip):
+        if re.search(self.port_pattern, target_ip):
             try:
                 print(urlparse(target_ip).netloc)
                 target_ip = gethostbyname(urlparse(target_ip).netloc)
