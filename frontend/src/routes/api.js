75b4f760d13b116c1416bd97fbae997b6813d27e
Joon <jeongjy0317@gmail.com>
2021-11-23 04:40:05 +0900

    Add:: Added handle of form object.

--- frontend/src/routes/api.js
+++ frontend/src/routes/api.js
@@ -1,9 +1,40 @@
 let express = require('express');
+let axios = require('axios');
 let router = express.Router();
 
+const sendErrorToUser = (res, status, message) => {
+    res.status(status).send({
+        message: message
+    });
+}
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.post('/job/enroll', function (req, res, next) {
+    // Send error if analysis option not presented
+    if (typeof (req.body.analysisOption) === "undefined") return sendErrorToUser(res, 400, "Bad request");
+    let analysisOption = JSON.parse(req.body.analysisOption);
+    console.log(analysisOption);
+
+    // Submit analysis option to job API
+    axios.post("http://localhost:20102/api/job", [{
+        targetURL: analysisOption.target.url,
+        knownInfo: analysisOption.info,
+        recursiveLevel: analysisOption.tool.analysisLevel,
+        uriPath: analysisOption.target.path,
+        done: false,
+        maximumProcess: "None"
+    }])
+        .then(jobDataSubmitRes => {
+
+            axios.post("http://localhost:20302/", analysisOption)
+                .then(jobSubmitRes => console.log("Done!"))
+                .catch(jobSubmitError => console.log("Fail! @ Final job submission", jobSubmitError.response.data));
+            res.send("Submitted!");
+        })
+        .catch(jobDataSubmitError => {
+            console.log("Fail! @ Job data submission!", jobDataSubmitError);
+        });
+    // res.send(req.body.analysisOption)
 });
 
 module.exports = router;
