16df316ebdefbfea476daa18b10b5b787cc6b3d9
Joon <jeongjy0317@gmail.com>
2021-11-23 21:57:21 +0900

    Hotfix:: Patched container-side request error

--- frontend/src/routes/api.js
+++ frontend/src/routes/api.js
@@ -16,7 +16,11 @@ router.post('/job/enroll', function (req, res, next) {
     console.log(analysisOption);
 
     // Submit analysis option to job API
-    axios.post("http://localhost:20102/api/job", [{
+    axios.get("http://bwasp-api-1:20102/api/job")
+        .then(res=>console.log("0 Success!", res))
+        .catch(fail=>console.log("1 Failed!", fail));
+
+    axios.post("http://bwasp-api-1:20102/api/job", [{
         targetURL: analysisOption.target.url,
         knownInfo: analysisOption.info,
         recursiveLevel: analysisOption.tool.analysisLevel,
@@ -25,14 +29,16 @@ router.post('/job/enroll', function (req, res, next) {
         maximumProcess: "None"
     }])
         .then(jobDataSubmitRes => {
-
-            axios.post("http://localhost:20302/", analysisOption)
-                .then(jobSubmitRes => console.log("Done!"))
-                .catch(jobSubmitError => console.log("Fail! @ Final job submission", jobSubmitError.response.data));
+            console.log("\n[ SUCCESS ] Job data submission \n");
+            axios.post("bwasp-core-1:20302", analysisOption)
+                .then(jobSubmitRes => console.log("\n[ SUCCESS ] Final job order \n"))
+                .catch(jobSubmitError => {
+                    console.log("\n[ FAIL ] Final job order \n================\n\n", jobSubmitError.response.data, "\n\n================")
+                });
             res.send("Submitted!");
         })
         .catch(jobDataSubmitError => {
-            console.log("Fail! @ Job data submission!", jobDataSubmitError);
+            console.log("\n[ FAIL ] Job data submission \n================\n\n", jobDataSubmitError, "\n\n================")
         });
     // res.send(req.body.analysisOption)
 });
