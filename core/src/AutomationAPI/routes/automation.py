f489e77311f79caf2121ad9fbf7ad951876eb19f
isanghyeon <cpd4268@naver.com>
2021-11-25 10:46:15 +0900

    Update automation.py

--- core/src/AutomationAPI/routes/automation.py
+++ core/src/AutomationAPI/routes/automation.py
@@ -15,6 +15,14 @@ bp = Blueprint(NAME, __name__, url_prefix='/')
 def DataReqRes():
     if request.method == 'POST':
         reqJsonData = request.get_json()
-        start(reqJsonData["target"]["url"], reqJsonData["tool"]["analysisLevel"], reqJsonData)
+        """
+        {"tool":{"analysisLevel":"1","optionalJobs":[]},"info":{"server":[],"framework":[],"backend":[]},"target":{"url":"http://logos.sch.ac.kr","path":[""]}}
+        """
+
+        try:
+            start(reqJsonData["target"]["url"], int(reqJsonData["tool"]["analysisLevel"]), reqJsonData)
+            return render_template_string("Done"), 200
+        except:
+            return abort(500, f"Automation Analysis Exception error; Your options: {reqJsonData}")
 
     return render_template_string("The BoB Web Application Security Project")
