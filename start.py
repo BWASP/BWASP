f9c2d4c1889f5880f5651bd28a6f68a8573828cd
dohunny <dhkoo.hacker@gmail.com>
2021-12-02 10:58:00 +0900

    start.py > ManualAPI/app.py 실행 코드 업데이트

--- start.py
+++ start.py
@@ -3,19 +3,14 @@
 import os, sys
 import webbrowser
 import subprocess
-from multiprocessing import Process
-
-def exec_server(name):
-    app = subprocess.Popen([sys.executable, name])
-
 
 if __name__ == '__main__':
-    # try:
-    proc_list = []
-    namelist =["Web/app.py","API/app.py","ManualAPI/app.py"]
-    webbrowser.open("http://localhost:20102")
-    webbrowser.open("http://localhost:20002")
-    for name in namelist:
-        proc = Process(target=exec_server,args=(name,))
-        proc.start()
-        proc_list.append(proc)
+    try:
+        webbrowser.open("http://localhost:20102")
+        webbrowser.open("http://localhost:20002")
+        app = subprocess.Popen([sys.executable, "Web/app.py"])
+        app2 = subprocess.Popen([sys.executable, "ManualAPI/app.py"])
+        subprocess.call(["python", "API/app.py"])
+    except KeyboardInterrupt:
+        app.terminate()
+        app2.terminate()
