4be0e97945742809385806067eb00d6ef9eccb85
isanghyeon <cpd4268@naver.com>
2021-12-03 21:53:51 +0900

    Update TASK.py

--- RestAPI/models/TASK_MANAGER/TASK.py
+++ RestAPI/models/TASK_MANAGER/TASK.py
@@ -24,41 +24,9 @@ class task(TASK_MANAGER_DB.Model):
     targetURL = TASK_MANAGER_DB.Column(TASK_MANAGER_DB.TEXT, nullable=False)
     task_id = TASK_MANAGER_DB.Column(TASK_MANAGER_DB.TEXT, nullable=False, unique=True)
 
-    def __init__(self, targetURL, task_id, done, **kwargs):
+    def __init__(self, targetURL, task_id, **kwargs):
         self.targetURL = targetURL
         self.task_id = task_id
 
     def __repr__(self):
         return f"<task('{self.targetURL}', '{self.task_id}', '{self.done}')>"
-
-
-"""
-{
-  "tool": {
-    "analysisLevel": 1624,
-    "optionalJobs": [
-      "CSPEvaluate",
-      "portScan",
-      "testPayloads"
-    ]
-  },
-  "info": [
-    {
-      "name": "PHP",
-      "version": "5.0"
-    },
-    {
-      "name": "MariaDB",
-      "version": "1.0"
-    }
-  ],
-  "target": "http://suninatas.com/",
-  "API": {
-    "google": {
-      "key": "1",
-      "engineId": "21"
-    }
-  },
-  "maximumProcess": 6
-}
-"""
