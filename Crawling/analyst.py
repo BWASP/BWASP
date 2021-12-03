e0ae7d7aeac5e066736f2b72f5d8aba83f09f0de
PecentZero <security428@korea.ac.kr>
2021-12-03 12:09:44 +0900

    sysinfo options parameter change

--- Crawling/analyst.py
+++ Crawling/analyst.py
@@ -1,4 +1,4 @@
 from Crawling.attack import sysinfo
 
 def start(detect_list,lock,target_url, req_res_packets, cur_page_links,current_url, packet_indexes,options):
-    return sysinfo.start(detect_list,lock,target_url, cur_page_links,current_url, req_res_packets,packet_indexes,options["framework"])
+    return sysinfo.start(detect_list,lock,target_url, cur_page_links,current_url, req_res_packets,packet_indexes,options)
