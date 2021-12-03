7434f93aed0e248e06f88d93ca610f69b1f1ff21
isanghyeon <cpd4268@naver.com>
2021-12-04 00:23:30 +0900

    Update start.py

--- start.py
+++ start.py
@@ -1,15 +1,15 @@
-#! BWASP/bin/python
+#! BWASP/bin/pytho
 
 import os, sys
 import webbrowser
 import subprocess
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         webbrowser.open("http://localhost:20102")
         webbrowser.open("http://localhost:20002")
         app = subprocess.Popen([sys.executable, "Web/app.py"])
-        app2 = subprocess.Popen([sys.executable, "RestAPI/app.py"])
+        subprocess.call(["python", "RestAPI/app.py"])
     except KeyboardInterrupt:
         app.terminate()
         app2.terminate()
