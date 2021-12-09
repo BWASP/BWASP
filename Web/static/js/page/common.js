4f648e0a1a9f23423532500c0812fee083c8cfac
Joon <jeongjy0317@gmail.com>
2021-12-09 18:55:25 +0900

    Patch:: Moved sidebar functionalities to common.js as module

--- Web/static/js/page/common.js
+++ Web/static/js/page/common.js
@@ -1,12 +1,47 @@
-let versionCheckModal = new bootstrap.Modal(document.getElementById('versionModal'), {
-    show: true
-});
+// Get modules
+import {Cookies, createToast} from '../jHelper.js';
 
-let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+let cookies = new Cookies(),
+    initialFrontConfig = {
+        dashboard: {
+            refreshRate: 3,
+            stopRequestWhenFailed: false
+        }
+    },
+    frontConfig = Object(),
+    targetCookieName = "frontConfig";
 
-document.getElementById("BWASP-Version").addEventListener("click", () => {
-    versionCheckModal.show();
-})
+// Get frontend config data from cookie
+while(true){
+    frontConfig = cookies.read(targetCookieName);
+
+    try{
+        frontConfig = JSON.parse(frontConfig);
+    }catch {
+        // createToast("Config validation fail", "Config JSON structure has corrupted.\nDeleted config", "danger");
+    }
+    console.log(frontConfig);
+
+    if(typeof(frontConfig) === "object") break;
+    else {
+        // createToast("Front config not found", "Generating with initial options");
+        cookies.create(targetCookieName, JSON.stringify(initialFrontConfig));
+    }
+}
+
+// Auto-activated Sidebar
+(() => {
+    if(window.location.pathname === "/") return;
+    let side = {
+        uri: window.location.pathname.split("/"),
+        target: "target"
+    };
+    side.uri.splice(0, 1);
+    side.uri.forEach(data => side.target += `-${data}`);
+
+    try{
+        document.getElementById(side.target).classList.add("active");
+    }catch (e){
+        console.error(e);
+    }
+})();
