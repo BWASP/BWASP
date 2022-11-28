1576af0a1bf50dd110f2acf38740db193864a3da
isanghyeon <cpd4268@naver.com>
2022-11-28 18:52:46 +0900

    Updated; Rest API db, models

--- RestAPI/models/BWASP/SYSTEMINFO.py
+++ RestAPI/models/BWASP/SYSTEMINFO.py
@@ -10,6 +10,7 @@
     :license: LICENSE_NAME, see LICENSE_FILE for more details.
 """
 import sys, os
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from .model_returnObj import bwasp_db as SYSTEMINFO_DB
@@ -22,10 +23,12 @@ class systeminfo(SYSTEMINFO_DB.Model):
     id = SYSTEMINFO_DB.Column(SYSTEMINFO_DB.Integer, primary_key=True, autoincrement=True)
     url = SYSTEMINFO_DB.Column(SYSTEMINFO_DB.TEXT, nullable=False)
     data = SYSTEMINFO_DB.Column(SYSTEMINFO_DB.TEXT, nullable=False)
+    subDomain = SYSTEMINFO_DB.Column(SYSTEMINFO_DB.TEXT, nullable=False)
 
-    def __init__(self, url, data, **kwargs):
+    def __init__(self, url, data, subDomain, **kwargs):
         self.url = url
         self.data = data
+        self.subDomain = subDomain
 
     def __repr__(self):
-        return f"<systeminfo('{self.url}', '{self.data}')>"
+        return f"<systeminfo('{self.url}', '{self.data}', '{self.subDomain}')>"
